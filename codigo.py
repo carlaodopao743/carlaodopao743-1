7da0919275cca528e2e645ea74468aa2dbabbac7
carlaodopao743 <camara.carlos@escola.pr.gov.br>
2025-05-20 10:33:19 -0300

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -1,1 +1,15 @@
+nome = str(input("nome: "))
+turma = str(input("turma: "))
+cgm = int(input("cgm: "))
 
+def cadastro_aluno(nome, turma, cgm):
+  print("O aluno foi cadastrado!")
+  opcao = input("Deseja ver cadastro? Digite 'S' p/ ver: ")
+  if opcao == 's' or opcao == 'S':
+   print(f"Suas informações são: {nome, turma, cgm}")
+  
+  else:
+    print("Seu cadastro foi concluido")
+
+
+cadastro_aluno(nome, turma,cgm)
